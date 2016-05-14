e0ea733962f831e7e2ede0ef216503345b40f355
Tyler Durden <durden@tamu.edu>
2016-05-14 00:01:35 -0500

    start and help commands

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-
+/*
 var express = require('express'),
   config = require('./config/config');
 
@@ -10,4 +10,34 @@ require('./config/express')(app, config);
 app.listen(config.port, function () {
   console.log('Express server listening on port ' + config.port);
 });
+*/
+
+var tg = require('telegram-node-bot')('192089181:AAE01YNBSlL80xnlWDNNSmKFdjOiIhSMCkE')
+
+tg.router.
+	when(['/start'], 'StartController').
+	when(['/help'], 'HelpController').
+	when(['ping'], 'PingController')
+
+tg.controller('StartController', ($) => {
+	var start_message = "Hi! I'm DurdenBot. Type /help to see what I can do. "
+
+	tg.for('/start', () => {
+		$.sendMessage(start_message)
+	})
+})
+
+tg.controller('HelpController', ($) => {
+	var help_message = "Type /help to see this message.\n"
+
+	tg.for('/help', () => {
+		$.sendMessage(help_message)
+	})
+})
+
+tg.controller('PingController', ($) => {
+	tg.for('ping', () => {
+		$.sendMessage('pong')	
+	})
+})
 
